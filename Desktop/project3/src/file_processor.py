310cbe51285147b5b99e4a2380c2226ecb1c08d3
hollylululu <hexxx544@gmail.com>
2018-02-27 23:03:13 -0800

    update positional inverted idnex

--- Desktop/project3/src/file_processor.py
+++ Desktop/project3/src/file_processor.py
@@ -2,43 +2,83 @@
 class file_processor:
 
 	def __init__ (self, filePath):
-		self.term_freq = {}
+		self.pos_inverted_index = {}
+		#self.docID = 
 		self.filePath = filePath
 		self.title = ''
 		self.body = ''
+		self.filePath = filePath
 
 
 	def split_body_title (self):
 		res_str = ''
 		with open(self.filePath, 'rt') as fin:
 			res_str = fin.read()
-		split_list = res_str.split('<body>')
-		self.title = split_list[0]
-		self.body = split_list[1].replace('</body>', '')
+
+		#some documents missing <body> tags. 
+		if '<body>' in res_str:
+			split_list = res_str.split('<body>')
+			self.title = split_list[0]
+			self.body = split_list[1].replace('</body>', '')
+			
+		else:
+			self.title = ''
+			self.body = res_str
+
 		return self.title, self.body
 
+"""return list of tuples: [(pos, word1), (pos, word2), (pos, word1), (pos, word3).....]
 
+"""
 
-	def tokenizer(self, str):
+	def tokenizer(self, inStr):
 		import nltk
 		import string
 		from tokenize import tokenize
+		from nltk.tokenize import WhitespaceTokenizer
+		import collections
+		from collections import OrderedDict
+		from collections import Counter
+
 		res_tokens = []
+		freq_dict = {}
 		translator = string.maketrans(string.punctuation, ' '*len(string.punctuation))
 
-		with open (self.filePath, 'r') as fin:
-			for line in fin:
-				line = line.replace("'","")
-				line = line.translate(translator).lower()
-				tokens = nltk.word_tokenize (line)
-				res_tokens.extend(tokens)
-		return res_tokens
+		outStr = inStr.replace("'","")
+		outStr = outStr.translate(translator).lower()
+		tempStr = outStr.split()
+
+		word_pos_list = list(enumerate(tempStr))
+		#tokens = nltk.word_tokenize(outStr)
+		#freq_dict = dict(collections.Counter(tokens))
+		return word_pos_list
+
+
+"""return dictonary: {term1: [pos1, pos2],term2: [pos1, pos2, pos3, ..., posN] }
+
+"""
+	def aggregate_pos_list (self, counter_list):
+		for tup in counter_list:
+			if tup[1] not in self.pos_inverted_index:
+				self.pos_inverted_index[tup[1]] = [tup[0]]
+			else:
+				self.pos_inverted_index[tup[1]].append(tup[0])
+		return self.pos_inverted_index
+
+
+		
+
+
+
+		
+"""
 
 	def computeWordFrequencies(self, tokens):
 		import collections
 		from collections import OrderedDict
 		from collections import Counter
 		return dict(collections.Counter(tokens))
+"""
 
 import argparse
 
@@ -49,9 +89,12 @@ def main():
 	args = parser.parse_args()
 
 	tester = file_processor(args.input_file)
-	body, title = tester.split_body_title()
+	title, body = tester.split_body_title()
 	tokens = tester.tokenizer(body)
-	print tester.computeWordFrequencies(tokens)
+	#print tokens
+	#print tester.computeWordFrequencies(tokens)
+
+	print tester.aggregate_pos_list(tokens)
 
 if __name__ == "__main__":
     main()
